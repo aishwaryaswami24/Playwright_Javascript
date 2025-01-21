3a423a64d261893c3a36386b3248c95f65df47e3
Aishwarya SwamiNagure <swamiaishwarya24@gmail.com>
2025-01-21 21:28:20 +0530

    datepicker

---
+++ tests/datepicker.spec.js
@@ -0,0 +1,49 @@
+const {test,expect}=require('@playwright/test')
+
+test('DatePicker',async ({page})=>{
+
+    await page.goto('https://testautomationpractice.blogspot.com/')
+
+    //await page.locator('#datepicker').fill('01/21/2025')
+
+    //date picker
+    const currentyear='2025'
+    const currentmonth='January'
+    const currentdate='21'
+
+    await page.click('#datepicker')
+
+    while(true){
+
+       
+        const year=await page.locator('.ui-datepicker-year').textContent()
+        const month=await page.locator('.ui-datepicker-month').textContent()
+
+        if( year==currentyear && month==currentmonth){
+
+            break;
+
+        }
+
+        await page.locator('//a[@title="Next"]').click()
+    }
+
+   
+    /* const dates=await page.$$('.ui-state-default')
+
+    for(let dt of dates){
+
+        if(await dt.textContent()==currentdate){
+            await dt.click()
+            break;
+        }
+
+    } */
+
+    await page.click('//a[@data-date="21"]')
+
+    await page.waitForTimeout(5000)
+
+    await page.close()
+
+})
